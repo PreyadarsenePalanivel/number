0881b0587cf0fadb3c1e1fcca76989e2cf8d518e
PreyadarsenePalanivel <darsenep@gmail.com>
2016-07-16 11:41:22 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,16 @@
+  public static void main(String[] args) {
+  
+        System.out.println("enter the number");
+        Scanner sc=new Scanner(System.in);
+        int i;
+                
+        i = sc.nextInt();
+        if(i%2==0)
+        {
+            System.out.println("even");
+    }
+        else 
+        {
+            System.out.println("odd");
+        }
+}
