60328b3a00ac78a601e12101f0f0683db6bfc48e
PreyadarsenePalanivel <darsenep@gmail.com>
2016-07-16 10:48:04 +0530

    Add files via upload

---
+++ pos or neg.java
@@ -0,0 +1,11 @@
+System.out.println("enter the number");
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        // TODO code application logic here
+        if(n>=0)
+        {
+            System.out.println("it is positive"+n);
+        }else if(n<=0)
+        {
+            System.out.println("it is negative"+n);
+        }
